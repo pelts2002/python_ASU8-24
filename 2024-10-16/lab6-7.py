710f0ffd5d61cd35019a44de256219d9f586dddd
Danil Pelts <you@example.com>
2024-10-22 09:27:22 +0500

    Решение Lab6-7

--- 2024-10-16/lab6-7.py
+++ 2024-10-16/lab6-7.py
@@ -10,44 +10,41 @@ class DNASequence:
         self._dna_sequence = dna_sequence.replace("\n", "").replace(" ", "").upper()    #убираем пробелы и перевод строк
         self._compressed_sequence = self._compress()    #сжимаем последовательность при инициализации
     
-    def _compress(self):    #приватный метод для сжатия ДНК
-        nucleotide_to_binary = {
-            'A': 0b00,
-            'C': 0b01,
-            'G': 0b10,
-            'T': 0b11
-        }
-        
+    def _compress(self):    #приватный метод для сжатия ДНК с использованием if,elif,else.
         compressed = 0
         for nuc in self._dna_sequence:
-            compressed <<= 2  #сдвиг влево на 2 бита для каждого нуклеотида
-            compressed |= nucleotide_to_binary[nuc]  #добавляем новые биты с помощью побитового ИЛИ
-        
+            compressed <<= 2    #сдвиг влево на 2 бита для каждого нуклеотида
+            if nuc == 'A':
+                compressed |= 0b00  # A -> 00
+            elif nuc == 'C':
+                compressed |= 0b01  # C -> 01
+            elif nuc == 'G':
+                compressed |= 0b10  # G -> 10
+            elif nuc == 'T':
+                compressed |= 0b11  # T -> 11
+            else:
+                raise ValueError(f"Недопустимый символ в последовательности ДНК: {nuc}")
         return compressed
     
-    def decompress(self):   #метод для распаковки данных
-        binary_to_nucleotide = {
-            0b00: 'A',
-            0b01: 'C',
-            0b10: 'G',
-            0b11: 'T'
-        }
-        
+    def decompress(self):   #метод распаковки данных в строку нуклеотидов.Побитово!
         decompressed = []
         compressed_copy = self._compressed_sequence
-        #обходим последовательность справа налево, извлекая по 2 бита (1 нуклеотид за раз)
         for _ in range(len(self._dna_sequence)):
-            # Извлекаем последние 2 бита и получаем нуклеотид
-            decompressed.append(binary_to_nucleotide[compressed_copy & 0b11])
-            compressed_copy >>= 2  # Сдвигаем число вправо на 2 бита
-        
-        return ''.join(reversed(decompressed))  # Обратно переворачиваем последовательность
+            last_two_bits = compressed_copy & 0b11
+            if last_two_bits == 0b00:
+                decompressed.append('A')
+            elif last_two_bits == 0b01:
+                decompressed.append('C')
+            elif last_two_bits == 0b10:
+                decompressed.append('G')
+            elif last_two_bits == 0b11:
+                decompressed.append('T')
+            compressed_copy >>= 2 
+        return ''.join(reversed(decompressed))  #обратно переворачиваем последовательность
     
-    def __str__(self):  #строковое представление распакованной последовательности ДНК.
+    def __str__(self):
         return self.decompress()
 
-
-# Тестирование класса
 dna_sequence = """
 ctcacctgcgacgtggttggctgtgacgaaatgattgccgagctaccgatcaggcccacc
 agagaataaaacacgcagaagtccgaaatattaactaacgcgtgaggaacatccttcacc
