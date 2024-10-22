48c97d8791bc704acb5680d5af4d73f0b777fd1b
Danil Pelts <you@example.com>
2024-10-22 09:12:48 +0500

    Решение Lab 3-5

--- 2024-10-16/lab3-5.py
+++ 2024-10-16/lab3-5.py
@@ -17,7 +17,9 @@ class DNASequence:
             'G': '10',
             'T': '11'
         }           #последовательность => бинарный вид
-        compressed = ''.join([nucleotide_to_binary[nuc.upper()] for nuc in self._dna_sequence])
+        binary_string = ''.join([nucleotide_to_binary[nuc] for nuc in self._dna_sequence])
+
+        compressed = int(binary_string, 2)  #из бинарной строки в целое число
         return compressed
     
     def decompress(self):   #метод для распаковки данных
@@ -26,9 +28,11 @@ class DNASequence:
             '01': 'C',
             '10': 'G',
             '11': 'T'
-        }    #разбиваем бинарную строку по 2 символа и декодируем
-        decompressed = ''.join([binary_to_nucleotide[self._compressed_sequence[i:i+2]] 
-                                for i in range(0, len(self._compressed_sequence), 2)])
+        }    #из целого число обратно в бинарную строку
+        binary_string = bin(self._compressed_sequence)[2:].zfill(len(self._dna_sequence) * 2)
+        
+        decompressed = ''.join([binary_to_nucleotide[binary_string[i:i+2]]  #разбиваем бинарную строку по 2 символа и декодируем
+                                for i in range(0, len(binary_string), 2)])
         return decompressed
     
     def __str__(self):  #представляем строкой
@@ -59,3 +63,5 @@ dna_obj = DNASequence(dna_sequence) #объект класса
 print("Распакованная последовательность:", str(dna_obj))
 
 print("Результат распаковки:", dna_obj.decompress())
+
+print("Сжатая последовательность (целое число):", dna_obj._compressed_sequence)
